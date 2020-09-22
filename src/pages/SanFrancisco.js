b887841b5a29e2081891423576c00d2459c708e2
rhysmaddren <rhysmaddren@hotmail.co.uk>
2020-09-22 17:38:03 +0100

    Added index, New York, San Fran and Yosemite pages

---
+++ src/pages/SanFrancisco.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function SanFran() {
+    return <div>
+
+
+        <h1>San Francisco</h1>
+
+        <article>
+            <p>
+                We attempted to see San Francisco in a single day. We started early, heading to pier 33 for our departure to Alcatraz island. Alcatraz was a great learning experience, however I felt that the reality of where I was didn’t sink in at the time because I was surrounded by other people. I imagine a night tour would more closely capture what it felt like to spend a day on the island whilst it was still operational. Nevertheless, Alcatraz absolutely remains a must see in San Francisco.
+            </p>
+
+            <p>
+                Upon arrival back, we explored Fisherman’s wharf and the famous pier 39. It was more expansive than we expected, offering a range of shops, restaurants and views of sea lions. Our only minor regret is that we should have tried a local eatery instead of opting for the safety of a chain restaurant, though we did get to watch the sea lions from our table.
+            </p>
+
+            <p>
+                After a thorough exploration of the gift shop, we headed back to the car and then to the golden gate bridge – perhaps one of the most iconic structures in the world. We made multiple stops, one on either side to the North of the bridge. Seeing something that’s so frequently depicted in film, in real life, is a surreal experience. Luckily, unlike most films, there wasn’t a disaster than caused it to collapse nor an ancient creature to knock it down, just incredible views.
+            </p>
+
+            <p>
+                As the sun began to set, we headed to Baker beach at the other side of the bridge. After an intense couple of days, it was nice to watch the sun set over the ocean. This also provided me with perhaps the best picture I’ve ever taken.
+            </p>
+
+            <p>
+                Once the sun had disappeared beneath the horizon, we took a detour to Lombard street. This was the first thrill of the trip, considering our rented SUV was almost the same width of some of the smaller turns. Fortunately, we emerged at the bottom unscathed, and headed to our first major surprise of the trip; twin peaks. The view from twin peaks was the most incredible view of the whole trip.
+            </p>
+
+            <p>
+                San Fran was like an amazing show that finished after 2 seasons, you’re glad you got to experience it, but you felt like there was so much more to see. Upon returning home, San Fran remains to be the only place I would immediately return to if I had the chance.
+            </p>
+        </article>
+
+
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+
+
+    <h1>Choose your trip</h1>
+
+    <Link to="/SanFrancisco">West coast</Link>
+    <Link to="/NewYork">East coast</Link>
+
+  </div>
 }
